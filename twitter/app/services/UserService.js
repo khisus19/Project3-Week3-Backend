0a2ba9f4c2847d00684b50ab8bee959492312311
Jesús Álvarez <khisus19@gmail.com>
2022-04-27 16:33:27 -0500

    Parte 4 Requerimiento 2 Prueba success

--- twitter/app/services/UserService.js
+++ twitter/app/services/UserService.js
@@ -4,6 +4,15 @@ class UserService {
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
+
+    static getInfo(user) {
+        let userInfoInList = []
+        userInfoInList.push(user.id)
+        userInfoInList.push(user.username)
+        userInfoInList.push(user.name)
+        userInfoInList.push(user.bio)
+        return userInfoInList
+    }
 }
 
 module.exports = UserService
