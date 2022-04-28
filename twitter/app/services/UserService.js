9d411dfe5aa032eac5352de1e7e951662dac9de6
Jesús Álvarez <khisus19@gmail.com>
2022-04-27 19:36:01 -0500

    Parte 4 Requerimiento 4 listo

--- twitter/app/services/UserService.js
+++ twitter/app/services/UserService.js
@@ -17,6 +17,17 @@ class UserService {
     static updateUserUsername(user, newUserName) {
         return user.username = newUserName
     }
+
+    static getAllUsernames() {
+        const user1 = UserService.create(1, "carlogilmar1", "Carlo")
+        const user2 = UserService.create(1, "carlogilmar2", "Carlo")
+        const user3 = UserService.create(1, "carlogilmar3", "Carlo")
+        const usernames = []
+        usernames.push(user1.username)
+        usernames.push(user2.username)
+        usernames.push(user3.username)
+        return usernames
+    }
 }
 
 module.exports = UserService
