657678a87e6f89802051437dc530eb21855bf2b4
Jesús Álvarez <khisus19@gmail.com>
2022-04-28 09:13:05 -0500

    P5 Requerimiento 1 listo

--- twitter/app/views/UserView.js
+++ twitter/app/views/UserView.js
@@ -1,13 +1,18 @@
-const User = require("./../models/user")
+const UserService = require('./../services/UserService')
 
-class UserView {
+class UserView{
+
     static createUser(payload){
-        const result = {payload: "payload no existe"}
-        
-        if (payload === null) {
-            return result.payload
+        if(payload === null) {
+            const result = {error: "El payload no existe"}
+            return result
+        } else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number') {
+            return UserService.create(payload.id, payload.username, payload.name);
+        } else{
+            return {error: "Error, las propiedades del payload necesitan tener un valor valido"};
         }
     }
 }
 
-module.export = UserView
+module.exports = UserView
+
